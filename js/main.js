24a56c52c23b63b097525a67d94800127a91c193
Pekka Okkonen <pekka.okkonen1@gmail.com>
2019-08-13 08:37:07 +0300

    added one layer to for loops

--- js/main.js
+++ js/main.js
@@ -2,10 +2,10 @@
 
 // Example object
 /*var object = {
-	
+
 	dates :
 		["27.7", "28.7", "29.7", "30.7", "31.7", "1.8", "2.8", "3.8", "4.8", "5.8", "6.8"],
-	
+
 	values : {
 		"name1" : [10, 9, 8, 7, 6, 5, 4, 3, 2, 1, " "],
 		"name2" : [10, 9, 8, 7, 6, 5, " ", 4, 3, 2, 1],
@@ -13,12 +13,22 @@
 	}
 }*/
 
+window.onload=function() {
+	var btn = document.createElement("BUTTON");
+	btn.innerHTML = "Clear widget";
+	document.body.appendChild(btn);
+	btn.onclick = async function() {
+		document.body.innerHTML = "";
+		document.body.appendChild(btn);
+	}
+}
+
 
 function createLinkButton(content) {
 	// Creates button
 	// onClick creates downloadable csv file
-	
-	
+
+
 	// Download file button
 	var linkBtn = document.createElement("BUTTON");
 	linkBtn.innerHTML = "download file";
@@ -38,13 +48,13 @@ function createTable(content) {
 	var table = document.createElement('table');
 	table.style.width = '100%';
 	table.setAttribtue('border', '1');
-}*/ 
+}*/
 
 function main(objectArg) {
 	/*
-		Takes object containing 
+		Takes object containing
 		dates : ["1.1", "2.1"],
-		
+
 		values : {
 			[{ name : name,
 				data : data,
@@ -52,35 +62,39 @@ function main(objectArg) {
 		}
 		Creates .csv file and provides downloadable file.
 	*/
-	
-	var maxSize = objectArg["dates"].length;
-	
-	var dates = objectArg["dates"];
-	
-	var objects = objectArg["values"];
-	
-	
+
+	console.log(objectArg)
+
+	var maxSize = objectArg.dates.length;
+
+	var dates = objectArg.dates;
+
+	var values = objectArg.values;
+
+
 	// var names = Object.keys(objectArg["values"]);
-	
+
 	var csvContent = "data:text/csv;charset=utf-8,";
 	csvContent += "\r\n" + "dates" + ",";
 
-	for(var i = 0; i < objects.length; i++) {
-		csvContent += objects[i].name + ",";
+	for(var i = 0; i < values.length; i++) {
+		for (var j = 0; j < values[i].length; j++) {
+			csvContent += values[i][j].name + ",";
+		}
 	}
 	csvContent += "\r\n";
 	for(var i = 0; i < maxSize; i++) {
 		csvContent += dates[i] + ",";
-		for(var j = 0; j < objects.length; j++) {
-			csvContent += objects[j].data[i] + ",";
+		for(var j = 0; j < values.length; j++) {
+			for (var k = 0; k < values[j].length; k++) {
+				csvContent += values[j][k].data[i] + ",";
+			}
 		}
 		csvContent += "\r\n";
 	}
 	var btn = createLinkButton(csvContent);
 	document.body.appendChild(btn);
-
 }
-
 
 
 // main(object);
